5ebf9640e4d46c7cfc3b6ae6bdac27038ec34f74
ar-mechx <ar-mechx@hotmail.com>
2020-07-12 20:23:03 +0000

    adding in Docker project

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath='mechx/api:api'
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run api --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/api 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=udacitypinzonimage
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username mechx
+docker tag api mechx/api:v1pro3
 
 # Step 3:
 # Push image to a docker repository
+docker push mechx/api:v1pro3
